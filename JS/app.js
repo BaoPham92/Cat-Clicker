66f023793980b7232f1741f19fc3f24ec163e46a
Cuza <baopham92@yahoo.com>
2018-10-03 17:53:51 -0400

    Image, listener for clicks, loop function, and id for img tag added.

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,29 @@
 class Player {
     constructor() {
         this.defaultScore = document.querySelector('#total-clicks').innerHTML;
+
+        this.image = document.getElementById('main-image');
+    }
+
+    // Update when the image is clicked.
+    update() {
+
+        this.image.addEventListener('click', function(e) {
+            let clickedTarget = e.target;
+
+            console.log(clickedTarget);
+        })
     }
 
     consoleLog() {
-        console.log(this.defaultScore);
+        console.log(this.defaultScore, this.image);
     }
 }
 
 const player = new Player();
 player.consoleLog();
+
+// Loop for event listeners that are apart of a class.
+(function loop() {
+    player.update();
+})(this)
