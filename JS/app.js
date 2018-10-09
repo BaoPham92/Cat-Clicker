f1a1449c6e466ebdf0ed6e987a55f37e09d67ecc
Cuza <baopham92@yahoo.com>
2018-10-08 20:41:44 -0400

    Created generators to increment #clicks based on correct image clicks.

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,9 @@
 class Player {
     constructor() {
-        this.currentScore = 0;
-        this.defaultScore = document.querySelector('#total-clicks');
 
         // Obtain images from HTML.
         this.images = document.querySelectorAll('.cat-image');
-        this.amountOfImages = {name: 'placeholder', age: "placeholder"};
+        this.amountOfImages = {};
     }
 
     // Update when the image is clicked.
@@ -13,12 +11,34 @@ class Player {
 
         this.howMany(); // Create var === number of images to contain/store info.
 
+        let imageObj = Object.keys(this.amountOfImages).length;
+
+        // Two following generators are to iterate object of stored nested objects variables. (this.amountOfImages)
+        function* generator() {
+            for (let i = 0; i < imageObj; i++) {
+                console.log(i);
+                yield i;
+            }
+        }
+
+        function* mapIterator(iterator, mapping) {
+            while (true) {
+                let result = iterator.next();
+                if (result.done) {
+                    break;
+                }
+                yield mapping(result.value);
+            }
+        }
+
         for (const image of this.images) {
 
             // Declarations
-            let addHeader = document.createElement("h4"), 
+            let addHeader = document.createElement("h4"),
                 text = document.createTextNode('The number of times you have clicked:');
-                addHeader.appendChild(text);
+            addHeader.appendChild(text);
+
+            let headerContainer = [];
 
             // Conditionals / Comparisons.
             let compareSrc = image.attributes.getNamedItem('src'),
@@ -26,34 +46,50 @@ class Player {
 
             // Image click listener for scores.
             image.addEventListener('click', (e) => {
+
                 let clickedTarget = e.target; // Event delegation.
-                console.log(clickedTarget);
 
-                clickedTarget === image ? this.currentScore += 1 : console.log('error');
-
+                // Conditional for checking / inserting h4 element containing click data.
                 (compareSrc === compareSrc) && !ifcontainsH4 ? image.parentElement.insertBefore(addHeader, image.parentElement.children[1]) : console.log('Parent element contains a h4 tag.');
-                // console.log(compareSrc === compareSrc); // Console.log confirmination message.
 
-                this.defaultScore.innerHTML = `Number of clicks: ${this.currentScore}`;
+                // Push elements to container if container !have element.
+                Array.from(document.querySelectorAll('h4')).forEach((index) => {
+                    !headerContainer.includes(index.nextElementSibling) ? headerContainer.push(index.nextElementSibling) : console.log('error');
+                })
+
+                // Declaration / and generators that trigger for clicks based on clicked target element.
+                let values = generator(), mapped = mapIterator(values, (i) => {
+
+                    // Conditional.
+                    const result =
+                        this.amountOfImages[i].element === clickedTarget ?
+                            (this.amountOfImages[i].clicks++ , headerContainer.filter(header => header === clickedTarget)[0]
+                                .previousElementSibling.textContent = `The number of times you have clicked: ` + this.amountOfImages[i].clicks)
+                            : console.log('error');
+
+                    console.log(headerContainer.filter(header => header === clickedTarget)[0], this.amountOfImages);
+
+                    return result;
+                });
+
+                console.log(mapped.next());
+                console.log(mapped.next());
 
             })
         }
-
     }
 
     // Declaration creator for future image additions to be clicked on.
     howMany() {
 
         // Creates the amount of variables for the amount of images.
-        for(let i = 0; i < this.images.length; i++) {
+        for (let i = 0; i < this.images.length; i++) {
 
             // Future images with class of 'cat-image' is now stored in their own variables in a array.
             this.amountOfImages[i] = {};
             this.amountOfImages[i].element = this.images[i];
             this.amountOfImages[i].clicks = 0;
-
         }
-        console.log(this.amountOfImages);
     }
 
     consoleLog() {
