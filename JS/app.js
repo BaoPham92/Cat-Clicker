75e96ffcc473fdf105f2a988fccfa583f51d5f7a
Cuza <baopham92@yahoo.com>
2018-10-13 22:08:46 -0400

    Created base layout.

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,20 @@
 // Here will lay the version 2 of the revisions.
 
+var variable = [];
 
 // Objects.
     // Modal
+    const modal = {
+        set: (name, data) => { localStorage.setItem(name, data)},
+
+        get: (variable) => { console.log(variable) }
+    };
 
     // Connector
+    const connector = {};
 
     // View
+    const view = {};
+
+    modal.set('Notes', 'Chapter one: Testing this might have worked.');
+    console.log(localStorage.getItem('Notes'));
