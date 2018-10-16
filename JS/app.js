1023e5feaeda663773e4324ee92cebc98a9a1c42
Cuza <baopham92@yahoo.com>
2018-10-16 13:56:31 -0400

    Removed cat5.jpeg && added modal > imageObj property && renamed images.

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,33 @@
-// Here will lay the version 2 of the revisions.
+document.addEventListener('DOMContentLoaded', function () {
 
-var variable = [];
+    // Objects.
 
-// Objects.
-    // Modal
-    const modal = {
-        set: (name, data) => { localStorage.setItem(name, data)},
+        // Modal
+        const modal = {
 
-        get: (variable) => { console.log(variable) }
-    };
+            init: () => {},
 
-    // Connector
-    const connector = {};
+            imageObj: {
+                0: { name: 'One', image: 'images/cat1.jpg', clicks: 0 },
+                1: { name: 'Two', image: 'images/cat2.jpg', clicks: 0 },
+                2: { name: 'Three', image: 'images/cat3.jpg', clicks: 0 },
+                3: { name: 'Four', image: 'images/cat4.jpg', clicks: 0 },
+                4: { name: 'Five', image: 'images/cat5.jpg', clicks: 0 }
+            },
 
-    // View
-    const view = {};
+            set: (name, data) => { localStorage.setItem(name, data) },
 
-    modal.set('Notes', 'Chapter one: Testing this might have worked.');
-    console.log(localStorage.getItem('Notes'));
+            get: (variable) => { localStorage.getItem(variable) }
+        };
+
+        // Connector
+        const connector = {};
+
+        // View
+        const view = {};
+
+        // modal.set('Notes', 'Chapter one: Testing this might have worked.');
+        // console.log(localStorage.getItem('Notes'));
+        
+        console.log('This should log the object of all image information.', modal.imageObj)
+})
