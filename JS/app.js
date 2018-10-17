e65dd0dcdf6ea5e8f76c578ac670b1045f9dacac
Cuza <baopham92@yahoo.com>
2018-10-17 15:48:35 -0400

    Mechanics for visible stats and obj information now displaying.

--- JS/app.js
+++ JS/app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         queryAll: (data) => { return document.querySelectorAll(data) },
 
         // Element creation.
-        creatEl: (data) => { return document.createElement(data)}
+        creatEl: (data) => { return document.createElement(data) }
     }
 
     // Modal
@@ -36,25 +36,41 @@ document.addEventListener('DOMContentLoaded', function () {
             viewImage.init();
         },
 
-        getListImg: (data, i) => {
-                data.getAttributeNode('name').value === modal.imageObj[i].name
-                ? domHelper.query('#image').setAttribute('src', modal.imageObj[i].image)
+        getListImg: (target, i) => {
+
+            let obj = modal.imageObj[i]
+
+            // Conditional to retreive and set images according to clicks.
+            target.getAttributeNode('name').value === obj.name
+                ? domHelper.query('#image').setAttribute('src', obj.image)
                 : console.log('Incorrect match.');
+
+            modal.image = obj // Set current image upon click selection.
+        },
+
+        getCurrentImg: () => {
+            return modal.image;
+        },
+
+        incrementClicks: () => {
+            modal.image.clicks++;
+            console.log(modal.image.clicks)
+            viewImage.render();
         }
     };
 
-    // View
+    // View containing list of clickable options.
     const viewList = {
         init: () => {
             let list = domHelper.query('#image-list');
-            
-            for(let i = 0; i < modal.imageObj.length; i++) {
+
+            for (let i = 0; i < modal.imageObj.length; i++) {
                 li = domHelper.creatEl('li');
                 li.textContent = i + 1;
                 li.setAttribute('name', modal.imageObj[i].name);
                 list.appendChild(li);
 
-                li.addEventListener('click', (e)=>{
+                li.addEventListener('click', (e) => {
                     let target = e.target;
 
                     connector.getListImg(target, i);
@@ -64,22 +80,47 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     };
 
+    // View containing information and stats.
     const viewImage = {
         init: () => {
 
             // Query and create elements.
-            let container = domHelper.query('#images'), 
-            img = domHelper.creatEl('img'), 
-            h2 = domHelper.creatEl('h2'), 
-            span = domHelper.creatEl('span');
-            
-            // Set attr and append elements to container.
-            img.setAttribute('id', 'image'), h2.setAttribute('id', 'image-name'), span.setAttribute('id', 'click');
-            container.appendChild(img);
-            container.appendChild(h2);
-            container.appendChild(span);
+            this.container = domHelper.query('#images'),
+                this.img = domHelper.creatEl('img'),
+                this.h2 = domHelper.creatEl('h2'),
+                this.span = domHelper.creatEl('span');
 
-            console.log(img,h2,h2); // Console.log for confirmination.
+
+            // Set attr and append elements to container.
+            this.img.setAttribute('id', 'image'),
+                this.h2.setAttribute('id', 'image-name'),
+                this.span.setAttribute('id', 'image-clicks');
+
+            this.container.appendChild(this.img);
+            this.container.appendChild(this.h2);
+            this.container.appendChild(this.span);
+
+            this.img.addEventListener('click', (e) => {
+                let target = e.target;
+
+                connector.incrementClicks();
+                console.log(target === this.img);
+            })
+            console.log(this.imgQ, this.h2Q, this.h2Q); // Console.log for confirmination.
+        },
+
+        render: () => {
+
+            // Query new appended elements.
+            this.imgQ = domHelper.query('#image'),
+                this.h2Q = domHelper.query('#image-name'),
+                this.spanQ = domHelper.query('#image-clicks');
+
+            // Adjust stats for image.
+            let currentImg = connector.getCurrentImg();
+            this.h2Q.textContent = currentImg.name;
+            this.spanQ.textContent = currentImg.clicks;
+            this.imgQ.src = currentImg.image;
         }
     }
 
